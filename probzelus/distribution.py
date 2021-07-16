af6ea03e7717f33e7864ea1e8b0ffd0eebd382bb
rtekin <rtekin>
2021-07-16 13:47:54 +0200

    Fix draw + define Support for categorical distributions * Short fix for draw : using builtin Python random to generate seeds. Using an attribut of the decorated functions leads sometimes to a Jax tracer leak error.

--- probzelus/distribution.py
+++ probzelus/distribution.py
@@ -1,10 +1,14 @@
-from distrax import Normal, Uniform, Bernoulli
+from distrax import Normal, Uniform, Bernoulli, Categorical
 from jax.random import PRNGKey
 from jax.random import split as jrd_split
 from jax.random import beta as jrd_beta
 from jax.random import poisson as jrd_poisson
+from jax.random import categorical
 from jax.scipy.special import gammaln
 import jax.numpy as np
+from jax.tree_util import tree_flatten, tree_unflatten, register_pytree_node_class, register_pytree_node
+from jax import tree_map
+import random as py_random
 
 def gaussian (mean, sigma2): return Normal(loc=mean, scale=np.sqrt(sigma2))
 def uniform_float (low, high): return Uniform(low=low, high=high)
@@ -48,6 +52,42 @@ class Poisson():
 def poisson (lambd) : return Poisson(lambd)
 
 
+@register_pytree_node_class
+class Support():
+    def __init__(self, values, logits):   
+        # TODO : MERGE
+        self.values = values
+        self.logits = logits
+        self.probs = self.logits
+    
+    def sample(self, seed):
+        idx = categorical(key=seed, logits=self.logits)
+        return np.take(self.values, idx)
+
+    def log_prob(self, val):
+        np.where(self.values==val)
+        return np.take(self.logits, idx)
+
+    def mean(self):
+        return np.average(self.values, weights=self.logits, axis=0)
+
+    def variance(self):
+        mean = self.mean()
+        return np.average(np.square(self.values - mean))
+
+    def block_until_ready(self):
+        tree_map(lambda x : x.block_until_ready(), self.values)
+        tree_map(lambda x : x.block_until_ready(), self.logits)
+        return
+    
+    ## JAX methods to be able to vectorize
+    def tree_flatten(self):
+        return ((self.values, self.probs), None)
+    @classmethod
+    def tree_unflatten(cls, aux_data, children):
+        return cls(*children)
+
+
 def score(distrib, x):
     return distrib.log_prob(x)
 
@@ -60,7 +100,7 @@ def seed(seed=0):
         return aux
     return decorator
 
-@seed()
+
 def draw(distrib, key=None):
     """
     If draw is vectorized, draw need an argument key in order to be able 
@@ -69,22 +109,29 @@ def draw(distrib, key=None):
     """
     def _draw(d, k):
         if k is None:
-            k = draw.key
+            k = PRNGKey(py_random.randint(0, 1000))
         return d.sample(seed=k)
     return _draw(distrib, key)
 
 
-def mean_float(t):
-    return np.average(t[:,0], weights=t[:,1])
+def mean_float(d):
+    return np.average(d.values, weights=d.probs, axis=0)
+mean_int = mean_float
+mean_bool = mean_float
 
 def stats_float(t):
     mean = mean_float(t)
     mu = np.average(np.square(t - mean))
     return mean, mu
 
-def split(x):
-    p = x[:,2]
-    return np.column_stack((x[:,0], p)), np.column_stack((x[:,1], p))
+def split(d):
+    p = d.probs
+    v = d.values
+    return Support(v[0], p), Support(v[1], p)
+
+def split_array(t):
+    return t
+
 
 # TODO : implement the Zelus functions bellow
 """
@@ -111,8 +158,6 @@ type 'a t (* = *)
   (* | Dist_mult of float t * float t *)
   (* | Dist_app : ('a -> 'b) t * 'a t -> 'b t *)
 
-val draw : 'a t -> 'a
-val score : 'a t * 'a -> float
 val draw_and_score : 'a t -> 'a * float
 
 val print_any_t : 'a t -AD-> unit
@@ -125,8 +170,6 @@ val stats_float_list : float list t -> (float * float) list
 val mean_float_list : float list t -> float list
 val mean : ('a -> float) -> 'a t -> float
 val mean_list : ('a -> float) -> 'a list t -> float list
-val mean_int : int t -> float
-val mean_bool : bool t -> float
 val mean_signal_present : 'a signal t -> float
 val mean_matrix: Mat.mat t -> Mat.mat
 
@@ -136,13 +179,11 @@ val bernoulli_draw : float -> bool
 val bernoulli_score : float -> bool -> float
 val bernoulli_mean : 'a -> 'a
 val bernoulli_variance : float -> float
-val bernoulli : float -> bool t
 
 val gaussian_draw : float -> float -> float
 val gaussian_score : float -> float -> float -> float
 val gaussian_mean : 'a -> 'b -> 'a
 val gaussian_variance : 'a -> float -> float
-val gaussian : float * float -> float t
 
 val mv_gaussian : Mat.mat * Mat.mat -> Mat.mat t
 val mv_gaussian_curried : Mat.mat -S-> Mat.mat -> Mat.mat t
@@ -159,7 +200,6 @@ val uniform_float_draw : float -> float -> float
 val uniform_float_score : float -> float -> float -> float
 val uniform_float_mean : float -> float -> float
 val uniform_float_variance : float -> float -> float
-val uniform_float : float * float -> float t
 
 val uniform_list : 'a list -> 'a t
 
@@ -177,7 +217,6 @@ val poisson_draw : float -> int
 val poisson_score : float -> float -> float
 val poisson_mean : float -> float
 val poisson_variance : float -> float
-val poisson : float -> int t
 
 val alias_method_unsafe : 'a array -> float array -> 'a t
 val alias_method_list : ('a * float) list -> 'a t
@@ -192,8 +231,6 @@ val to_dist_support : 'a t -> 'a t
 val of_list : 'a t list -> 'a list t
 val of_pair : 'a t * 'b t -> ('a * 'b) t
 val split : ('a * 'b) t -> 'a t * 'b t
-val split_array : 'a array t -> 'a t array
-val split_matrix : 'a array array t -> 'a t array array
 val split_list : 'a list t -> 'a t list
 val to_mixture : 'a t t -> 'a t
 val to_signal : 'a signal t -> 'a t signal
